9d8c2053d49d6902bb67bf6bae923f1b64c0e41f
TuongNT <tuongtf@gmail.com>
2021-12-03 00:32:48 +0700

    add bootstrap 4

--- src/index.js
+++ src/index.js
@@ -4,17 +4,28 @@ const morgan = require("morgan");
 const path = require('path');
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname,'public')));
+
 // HTTP Logger
 app.use(morgan('combined'));
 
 //Template Engine
-app.engine('handlebars' , exphbs.engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs' , exphbs.engine(
+  {
+    extname : '.hbs'
+  }
+));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resoures/views'));
 
 //Route
 app.get("/", (req, res) => {
   res.render('home');
+});
+
+app.get("/news", (req, res) => {
+  res.render('news');
 });
 
 app.listen(port, () => {
